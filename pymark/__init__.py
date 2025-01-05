6f459c07b3c9c0afafc6da780599350b5d65309a
コマリン親衛隊 <masato190411@icloud.com>
2025-01-06 07:54:41 +0900

    Update __init__.py

--- pymark/__init__.py
+++ pymark/__init__.py
@@ -1,4 +1,13 @@
 from ._pymark import convert
+import ctypes
+import os
+from pathlib import Path
 
 
 __all__ = ("convert",)
+
+
+lib_dir = Path(__file__).parent
+lib_path = lib_dir / "libcmark.so"
+
+ctypes.CDLL(str(lib_path))
